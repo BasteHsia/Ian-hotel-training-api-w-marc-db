6972ca668b4a9db87948a8f274e64bee5fc1aec9
BasteHsia <sebastianhsia.io@gmail.com>
2026-05-04 18:10:19 +0800

    edit code

--- Ian-getBookings.js
+++ Ian-getBookings.js
@@ -8,12 +8,24 @@ exports.handler = async () => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS"
+      },
       body: JSON.stringify(result.rows),
     };
 
   } catch (err) {
     return {
       statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS"
+      },
       body: JSON.stringify({ message: err.message }),
     };
   }
